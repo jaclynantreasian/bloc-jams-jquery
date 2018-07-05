53d193ea0a13e04aafd0d256c9a9b26183659e1a
Jaclyn Antreasian <jaclynantreasian@Jaclyns-MBP.fios-router.home>
2018-07-05 13:33:46 -0400

    checkpoint 10 submission completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -5,7 +5,6 @@
         $('button#play-pause').attr('playState', player.playState)
         $('button#next').on('click', function(){
             if(player.playState !== 'playing') { return }
-
             const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
             const nextSongIndex = currentSongIndex + 1;
             if (nextSongIndex>= album.songs.length) { return; }
@@ -14,3 +13,20 @@
         });
     });
 }
+
+$('button#previous').on('click', function(){
+    //previous and next track only respond when a song is currently currentlyPlaying
+
+    if (player.playState !== 'playing') { return; }
+
+    // previous track button plays the previous song on the Album
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = currentSongIndex - 1;
+
+        //previous track button doesn't respond if there is no previous song
+        if (prevSongIndex <= album.songs.length) { return; }
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
+
+});
