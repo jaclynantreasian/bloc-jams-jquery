f9b0c75680a770a641edf51c6e80ca8afea19cbc
Jaclyn Antreasian <jaclynantreasian@Jaclyns-MBP.fios-router.home>
2018-07-06 20:50:03 -0400

    player-bar fix

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,20 +13,6 @@
             const nextSongIndex = currentSongIndex + 1;
             if (nextSongIndex>= album.songs.length) { return; }
             const nextSong = album.songs[nextSongIndex];
-<<<<<<< HEAD
-            player.playPause(nextSong);
-
-        });
-
-        $('button#previous').on('click', function(){
-                if (player.playState !== 'playing') { return; }
-                const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-                const prevSongIndex = currentSongIndex - 1;
-                if (prevSongIndex < 0) { return; }
-                const prevSong = album.songs[prevSongIndex];
-                player.playPause(prevSong);
-        });
-=======
             helper.playPauseAndUpdate(nextSong);
 
         });
@@ -56,5 +42,4 @@
  $('#volume-control input').on('input', function (event){
        player.setVolume(event.target.value)
    });
->>>>>>> checkpoint-11-sliders
 }
