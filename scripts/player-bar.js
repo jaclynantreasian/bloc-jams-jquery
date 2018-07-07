9aadf308c929a731e55a2ceac55703c8759b71d8
Jaclyn Antreasian <jaclynantreasian@Jaclyns-MBP.fios-router.home>
2018-07-06 20:33:03 -0400

    checkpoint 11, Bloc Jams complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,32 +1,45 @@
 {
     $('button#play-pause').on('click', function(){
-        player.playPause();
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const song = album.songs[currentSongIndex];
+        helper.playPauseAndUpdate(song);
         $(this).attr('playState', player.playState);
-        $('button#play-pause').attr('playState', player.playState)
+        $('button#play-pause').attr('playState', player.playState);
+    });
+
         $('button#next').on('click', function(){
-            if(player.playState !== 'playing') { return }
+            if (player.playState !== 'playing') { return; }
             const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
             const nextSongIndex = currentSongIndex + 1;
             if (nextSongIndex>= album.songs.length) { return; }
             const nextSong = album.songs[nextSongIndex];
-            player.playPause(nextSong);
+            helper.playPauseAndUpdate(nextSong);
+
         });
-    });
+
+        $('button#previous').on('click', function(){
+                if (player.playState !== 'playing') { return; }
+                const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+                const prevSongIndex = currentSongIndex - 1;
+                if (prevSongIndex < 0) { return; }
+                const prevSong = album.songs[prevSongIndex];
+                helper.playPauseAndUpdate(prevSong);
+        });
+
+        $('#time-control input').on('input', function (event) {
+             player.skipTo(event.target.value);
+           });
+
+   setInterval( () => {
+       if (player.playState !== 'playing') { return; }
+       const currentTime = player.getTime();
+       const duration = player.getDuration();
+       const percent = (currentTime / duration) * 100;
+       $('#time-control .current-time').text( player.prettyTime(currentTime));
+       $('#time-control input').val(percent);
+   }, 1000);
+
+ $('#volume-control input').on('input', function (event){
+       player.setVolume(event.target.value)
+   });
 }
-
-$('button#previous').on('click', function(){
-    //previous and next track only respond when a song is currently currentlyPlaying
-
-    if (player.playState !== 'playing') { return; }
-
-    // previous track button plays the previous song on the Album
-        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-        const prevSongIndex = currentSongIndex - 1;
-
-        //previous track button doesn't respond if there is no previous song
-        if (prevSongIndex <= album.songs.length) { return; }
-
-        const prevSong = album.songs[prevSongIndex];
-        player.playPause(prevSong);
-
-});
